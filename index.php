91d4328ec8950cf17b3e46c745b75d7f441ea3ba
wizgrao <wizgrao@gmail.com>
2017-06-08 04:07:07 -0400

    Test database connection

--- index.php
+++ index.php
@@ -6,4 +6,11 @@
  * Time: 3:38 AM
  */
 require_once (dirname(__FILE__,2).'/config.php');
-echo TEST;
+$connection = mysqli_connect(DBHOST,DBLOGIN,DBPASS,DBNAME);
+$query = "SELECT id from blog;";
+$statement = $connection->prepare($query);
+$statement->execute();
+$statement->bind_result($id);
+while($statement->fetch()){
+    echo $id;
+}
