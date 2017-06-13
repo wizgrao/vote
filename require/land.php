05d71964c7af5247ab3e5334e9bd2927bac3cb80
wizgrao <wizgrao@gmail.com>
2017-06-13 00:47:18 -0400

    fix Twig

--- require/land.php
+++ require/land.php
@@ -6,7 +6,6 @@
  * Time: 3:41 PM
  */
 
-Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem("twigs");
 $environ = new Twig_Environment($loader,array());
 echo $environ->render("base.twig",array("userNum"=>$_SESSION['vid']));
