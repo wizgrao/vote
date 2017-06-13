3dd6d591f0791a96ee4dcce496fbd02077865e07
wizgrao <wizgrao@gmail.com>
2017-06-13 00:54:26 -0400

    fix path for twig folder

--- require/land.php
+++ require/land.php
@@ -6,6 +6,6 @@
  * Time: 3:41 PM
  */
 
-$loader = new Twig_Loader_Filesystem("twigs");
+$loader = new Twig_Loader_Filesystem("require/twigs");
 $environ = new Twig_Environment($loader,array());
 echo $environ->render("base.twig",array("userNum"=>$_SESSION['vid']));
