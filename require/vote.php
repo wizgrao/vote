b3260f1d1a86897999507d96e4365719b662b6ae
wizgrao <wizgrao@gmail.com>
2017-07-02 00:37:41 -0400

    Create ballot code

--- require/vote.php
+++ require/vote.php
@@ -11,4 +11,32 @@
 
 $loader = new Twig_Loader_Filesystem("require/twigs");
 $environ = new Twig_Environment($loader,array());
-echo $environ->render("vote.twig",array("userNum"=>$_SESSION['vid']));
+
+$error = array();
+$candidates = array();
+$numCandidates =0;
+if(!isset($_GET['election'])){
+    $error[] = "No election set";
+}else{
+    $connection = mysqli_connect(DBHOST,DBLOGIN,DBPASS,DBNAME);
+
+    $query = "SELECT elections.name as electionName, choices.name as candidateName, choices.id as candidateID  FROM elections, choices WHERE elections.id = choices.election AND elections.id = ? ";
+    $statement = $connection->prepare($query);
+    $statement->bind_param("i", $_GET['election']);
+    $statement->execute();
+    $statement->bind_result($electionN, $candidateName, $candidateID);
+    $numCandidates =0;
+    $candidates = array();
+    $electionName = "";
+
+    while($statement->fetch()){
+        $numCandidates++;
+
+        $candidates[] = array("name"=>$candidateName, "id"=>$candidateID);
+        $electionName = $electionN;
+    }
+
+
+}
+
+echo $environ->render("vote.twig",array("userNum"=>$_SESSION['vid'], "errors"=>$error,"candidates" =>$candidates, "numCandidates"=>$numCandidates, "electionName"=>$electionName));
