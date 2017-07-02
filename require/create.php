91c2eb380ccc130f83f4a45045409b5aecf87eaa
wizgrao <wizgrao@gmail.com>
2017-07-01 21:53:26 -0400

    Add code to handle new elections

--- require/create.php
+++ require/create.php
@@ -5,13 +5,37 @@
  * Date: 6/8/2017
  * Time: 3:33 PM
  */
-$connection = mysqli_connect(DBHOST,DBLOGIN,DBPASS,DBNAME);
-
+$loader = new Twig_Loader_Filesystem("require/twigs");
+$environ = new Twig_Environment($loader,array());
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    require ("view.php");
+    $connection = mysqli_connect(DBHOST,DBLOGIN,DBPASS,DBNAME);
+
+
+
+    $query = "INSERT INTO elections (name, choices) VALUES (?,?)";
+
+    $statement = $connection->prepare($query);
+    $statement->bind_param("si", $_POST['name'], $numChoices);
+    $numChoices = array_count_values($_POST['candidate']);
+    $statement->execute();
+    $electionID = $statement->insert_id;
+
+    $statement->close();
+
+
+    $candidateQuery = "INSERT INTO choices (name, election) VALUES (?,?)";
+
+    $candidateStatement = $connection->prepare($candidateQuery);
+    $candidateStatement->bind_param('si',$candidateName, $electionID);
+    foreach ($_POST['candidate'] as $candidateName)
+        $candidateStatement->execute();
+
+    $candidateStatement->close();
+    $connection->close();
+
+    echo $environ->render("createLand.twig",array("userNum"=>$_SESSION['vid'], "electionID"=>$electionID));
 }else{
-    $loader = new Twig_Loader_Filesystem("require/twigs");
-    $environ = new Twig_Environment($loader,array());
+
     echo $environ->render("create.twig",array("userNum"=>$_SESSION['vid']));
 }
 
